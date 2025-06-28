761c85360ed2dc619eaa7168167f1112c30dade5
Silven Mohan <silven667@gmail.com>
2025-06-28 14:06:21 +0530

    Initial Commit

---
+++ Mirror Check.c
@@ -0,0 +1,25 @@
+/** Palindrome Checkers for plain strings **/
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+main()
+{
+	char input1[100], input[100], rev[100];
+	system("cls");
+	printf("Enter the word:");
+	gets(input1);
+	strlwr(input1);
+	strcpy(input, input1);
+	strcpy(rev, input);
+	strrev(rev);
+	if(strcmp(rev, input)==0)
+	{
+		printf("%s is a Palindrome.", input);
+	}
+	else
+	{
+		printf("%s is not a Palindrome.", input);
+	}
+}
