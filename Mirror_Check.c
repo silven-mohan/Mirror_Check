97480a89f04b24fb4d07c8a82771355f04986c08
Silven Mohan <silven667@gmail.com>
2025-07-04 17:06:11 +0530

    Update Mirror_Check.c

--- Mirror_Check.c
+++ Mirror_Check.c
@@ -4,6 +4,25 @@
 #include<string.h>
 #include<stdlib.h>
 
+void strlwr(char *str) 
+{
+    for (int i = 0; str[i]; i++) 
+    {
+        str[i] = tolower(str[i]);
+    }
+}
+
+void strrev(char *str) 
+{
+    int i, len = strlen(str);
+    for (i = 0; i < len / 2; i++) 
+    {
+        char temp = str[i];
+        str[i] = str[len - i - 1];
+        str[len - i - 1] = temp;
+    }
+}
+
 int main()
 {
 	char input1[100], input[100], rev[100];
